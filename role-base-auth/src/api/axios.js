051df099f2f4111135f07d97ad0253ff6642ba13
sajeelzafar <sajeelzafar1995@gmail.com>
2024-02-22 12:53:28 +0500

    Staging Version

--- role-base-auth/src/api/axios.js
+++ role-base-auth/src/api/axios.js
@@ -3,7 +3,7 @@ const PRODUCTION_URL = "https://jazzcashbackendsajeel.onrender.com/";
 const BASE_URL = "http://localhost:3500";
 const ZAHID_URL = "http://bo-portal-bk.jazzcash.com.pk"
 export default axios.create({
-    baseURL: ZAHID_URL,
+    baseURL: BASE_URL,
     headers: { "Content-Type": "application/json" },
 });
 
